989812da86de49d215c897937000f45062c8b750
daadreeam-company <daadreeam@gmail.com>
2019-08-14 19:26:53 +0800

    熔断器 进行中

--- user-consumer/src/main/java/com/lsw/userconsumer/dao/UserDao.java
+++ user-consumer/src/main/java/com/lsw/userconsumer/dao/UserDao.java
@@ -1,19 +1,34 @@
 package com.lsw.userconsumer.dao;
 
 import com.lsw.userconsumer.pojo.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@Slf4j
 public class UserDao {
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "queryUserByIdFallback")
     public User queryUserById(Long id){
-        String url = "http://localhost:8081/user/" + id;
+        long begin = System.currentTimeMillis();
+        String url = "http://user-service/user/" + id;
+        User user = this.restTemplate.getForObject(url, User.class);
+        long end = System.currentTimeMillis();
+        // 记录访问用时：
+        log.info("访问用时：{}", end - begin);
+        return user;
+    }
 
-        return this.restTemplate.getForObject(url, User.class);
+    public User queryUserByIdFallback(Long id){
+        User user = new User();
+        user.setId(id);
+        user.setName("用户信息查询出现异常！");
+        return user;
     }
 }
